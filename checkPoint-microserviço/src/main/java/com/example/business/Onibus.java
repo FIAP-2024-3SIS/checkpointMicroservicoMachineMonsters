9b10246923a816f15ddcca61185c6a41428e4bf6
Lucas Dias <lucas.dias.moro@gmail.com>
2024-03-27 21:48:07 -0300

    Update Onibus.java

---
+++ checkPoint-microserviço/src/main/java/com/example/business/Onibus.java
@@ -0,0 +1,9 @@
+package com.example.business;
+
+public class Onibus{
+    private int idOnibus;
+    private String placa;
+    private String empresa;
+    private Motorista motorista;
+    private int qtdAssentos;
+}
