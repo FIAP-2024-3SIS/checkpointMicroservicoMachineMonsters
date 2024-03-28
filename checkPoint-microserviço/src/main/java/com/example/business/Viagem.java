f5d171dbb86ec79cce2a3033a19e63d43cfa3dc5
Eduardo de Souza <eduardo.souza01@outlook.com>
2024-03-27 22:03:31 -0300

    Att Classe Viagem e metodo Calcular e transformar

--- checkPoint-microserviço/src/main/java/com/example/business/Viagem.java
+++ checkPoint-microserviço/src/main/java/com/example/business/Viagem.java
@@ -3,14 +3,29 @@ public class Viagem{
 
     private String pontoDeChegada;
     private String pontoDePartida;
-    private double distancia;
-    private double velocidadeMedia;
+    private double distancia = 160;
+    private double velocidadeMedia = 100;
     Onibus onibus;
     Cliente cliente;
 
     public double calculoTempoViagem(){
 
+        double resultado;
 
+        resultado = distancia / velocidadeMedia;
+
+        return resultado;
+    }
+
+    public String converterTempo(){
+
+        double minutos = 0.6;
+
+        delta = calculoTempoViagem();
+
+        String resultado = delta * 0.6;
+
+        
 
     }
     
