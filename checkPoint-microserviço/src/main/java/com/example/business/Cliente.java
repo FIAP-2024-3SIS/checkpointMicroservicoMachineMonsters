e409b1410073964bfd4cace5424c06dd4759b300
Vitor Avellar <vitor07sa@gmail.com>
2024-03-27 21:46:16 -0300

    Classe Cliente
    Criacao da classe Cliente

---
+++ checkPoint-microserviço/src/main/java/com/example/business/Cliente.java
@@ -0,0 +1,17 @@
+package com.example.business;
+
+import org.apache.logging.log4j.message.StringFormattedMessage;
+
+public class Cliente {
+
+private String cpf;
+private String nome;
+private String rg;
+private String sexo;
+private int idade;
+
+
+
+
+
+} 
