e9386038d3b028205ff20dc28d32db310f610f98
Eduardo de Souza <eduardo.souza01@outlook.com>
2024-03-27 21:48:46 -0300

    Add Class Viagem

---
+++ checkPoint-microserviço/src/main/java/com/example/business/Viagem.java
@@ -0,0 +1,14 @@
+
+public class Viagem{
+
+    private String pontoDeChegada;
+    private String pontoDePartida;
+    private double distancia;
+    private double velocidadeMedia;
+    private
+    private
+    
+
+}
+
+
