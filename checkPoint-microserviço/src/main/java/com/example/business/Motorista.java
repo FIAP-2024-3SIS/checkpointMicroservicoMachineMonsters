0f5c6c46165c0befb1df18bce85c44e5f26dcd26
Osvaldo José Sandoli Neto <osvaldosandoliPC@gmail.com>
2024-03-27 21:44:00 -0300

    Classe Motorista
    Adicionando a classe Motorista

---
+++ checkPoint-microserviço/src/main/java/com/example/business/Motorista.java
@@ -0,0 +1,9 @@
+package com.example.business;
+
+public class Motorista {
+    private String CPF;
+    private String nome;
+    private String CNH;
+    private String sexo;
+    private int idade;
+}
