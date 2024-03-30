72744db1c660ff746589bbcbb3997a2a8089559a
dusouza01 <eduardo.souza01@outlook.com>
2024-03-30 12:55:45 -0300

    Main Att

---
+++ checkPoint-microserviço/src/main/java/com/example/mainTeste/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        
+        RegraTempo regra  = new RegraTempo();
+
+        System.out.println(regra.converterTempo());
+    }
+
+}
